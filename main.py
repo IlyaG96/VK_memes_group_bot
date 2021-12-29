35bb18c5bfb589abe787149ed56c16f3c5e445d3
IlyaG96 <ilyag_96@icloud.com>
2021-12-29 17:38:58 +1100

    remove pipelines

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def count_xkcd_comics():
     url = "https://xkcd.com/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
-
     total_comics = response.json()["num"]
 
     return total_comics
@@ -52,9 +51,9 @@ def get_vk_upload_url(vk_token, group_id, vk_api_version):
     return upload_url
 
 
-def send_to_server(photo_path, upload_url):
+def send_to_server(path_to_photo, upload_url):
 
-    with open(f"{photo_path}/photo.png", "rb") as file:
+    with open(path_to_photo, "rb") as file:
         files = {
             "file1": file,
         }
@@ -64,18 +63,13 @@ def send_to_server(photo_path, upload_url):
     return response.json()
 
 
-def save_on_server(photo_path,
-                   upload_url,
-                   vk_token,
+def save_on_server(vk_token,
                    user_id,
                    group_id,
-                   vk_api_version):
-
-    downloaded_photo_params = send_to_server(photo_path, upload_url)
-
-    vk_hash = downloaded_photo_params["hash"]
-    photo = downloaded_photo_params["photo"]
-    server = downloaded_photo_params["server"]
+                   vk_api_version,
+                   vk_hash,
+                   photo,
+                   server):
 
     payload = {
         "access_token": vk_token,
@@ -93,16 +87,24 @@ def save_on_server(photo_path,
     return response.json()
 
 
-def post_photo(vk_token, user_id, group_id, vk_api_version, photo_path, upload_url, photo_description, photo_title):
+def find_media_id(server_response):
 
-    server_response = save_on_server(photo_path, upload_url, vk_token, user_id, group_id, vk_api_version)
+    for answer in server_response["response"]:
+        media_id = answer["id"]
+
+    return media_id
+
+
+def write_message(photo_title, photo_description):
 
     message = f"{photo_title}\n\n" \
               f"{photo_description}\n\n" \
               f"by https://xkcd.com"
 
-    for answer in server_response["response"]:
-        media_id = answer["id"]
+    return message
+
+
+def post_photo(vk_token, user_id, group_id, vk_api_version, message, media_id):
 
     payload = {
         "access_token": vk_token,
@@ -119,6 +121,7 @@ def post_photo(vk_token, user_id, group_id, vk_api_version, photo_path, upload_u
 
 
 def remove_comic(path_to_photo):
+
     file_to_rem = pathlib.Path(path_to_photo)
     file_to_rem.unlink()
 
@@ -148,7 +151,19 @@ def main():
     download_comic(photo_link, path_to_photo)
 
     upload_url = get_vk_upload_url(vk_token, group_id, vk_api_version)
-    post_photo(vk_token, user_id, group_id, vk_api_version, photo_path, upload_url, photo_description, photo_title)
+
+    downloaded_photo_params = send_to_server(path_to_photo, upload_url)
+
+    vk_hash = downloaded_photo_params["hash"]
+    photo = downloaded_photo_params["photo"]
+    server = downloaded_photo_params["server"]
+
+    server_response = save_on_server(vk_token, user_id, group_id, vk_api_version, vk_hash, photo, server)
+    media_id = find_media_id(server_response)
+    message = write_message(photo_title, photo_description)
+
+    post_photo(vk_token, user_id, group_id, vk_api_version, message, media_id)
+
     remove_comic(path_to_photo)
 
 
